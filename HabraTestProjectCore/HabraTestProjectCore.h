2c6b2b54748c2f3c730f31e2e6519a4273eb9f30
Alexandr Babenko <Hikto583004@list.ru>
2017-05-09 18:34:32 +0600

    Initial Commit

---
+++ HabraTestProjectCore/HabraTestProjectCore.h
@@ -0,0 +1,19 @@
+//
+//  HabraTestProjectCore.h
+//  HabraTestProjectCore
+//
+//  Created by Alexandr Babenko (HuktoDev) on 09.05.17.
+//  Copyright © 2017 Alexandr Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HabraTestProjectCore.
+FOUNDATION_EXPORT double HabraTestProjectCoreVersionNumber;
+
+//! Project version string for HabraTestProjectCore.
+FOUNDATION_EXPORT const unsigned char HabraTestProjectCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HabraTestProjectCore/PublicHeader.h>
+
+
