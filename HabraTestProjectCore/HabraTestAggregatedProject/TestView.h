d75578d902710053fa1115d34db0a38d9135ea64
Alexandr Babenko <Hikto583004@list.ru>
2017-05-09 19:45:07 +0600

    Add TestView

---
+++ HabraTestProjectCore/HabraTestAggregatedProject/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  HabraTestProjectCore
+//
+//  Created by Alexandr Babenko (HuktoDev) on 09.05.17.
+//  Copyright © 2017 Alexandr Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ HabraTestProjectCore/HabraTestAggregatedProject/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  HabraTestProjectCore
+//
+//  Created by Alexandr Babenko (HuktoDev) on 09.05.17.
+//  Copyright © 2017 Alexandr Babenko. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
